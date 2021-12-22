41c1a6e81b777997e0f6edfb2294f8430bdd8e40
Valery Milovanov <milovanov.valeri@yandex.ru>
2021-12-22 16:11:34 +0400

    путь к файлам с картинками settings(MEDIA_URL,MEDIA_ROOT)

--- landingpagewebsite/settings.py
+++ landingpagewebsite/settings.py
@@ -123,6 +123,9 @@ STATICFILES_DIRS = [
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+MEDIA_URL = 'static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
